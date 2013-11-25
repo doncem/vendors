371659a745c153456ee8c761661ad296fa17733c
Donatas Martinkus <donatasmart@gmail.com>
2013-11-25 22:29:29 +0000

    updating github repositories for js libraries

--- combine-libraries.sh
+++ combine-libraries.sh
@@ -3,8 +3,9 @@
 cd libraries/
 
 echo "--- LINKING EXTERNAL LIBRARIES ---"
-ln -s ../source-libraries/backbone/backbone.js backbone.js
+ln -s ../source-libraries/backbone/ backbone
 ln -s ../source-libraries/jquery-color/jquery.color.js jquery.color.js
 ln -s ../source-libraries/jquery-flot/jquery.flot.js jquery.flot.js
+ln -s ../source-libraries/raphael/ raphael
 ln -s ../source-libraries/twitter-bootstrap/dist/ twbs
-ln -s ../source-libraries/underscore/underscore.js underscore.js
+ln -s ../source-libraries/underscore/ underscore
